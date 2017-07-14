0299805e30afd946b57b7141f7ba9e2886c5752f
Owain Llewellyn <owain.llew@gmail.com>
2017-07-14 17:20:28 -0400

    Added functionality for menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,14 +3,14 @@
  *  Web Starter Kit
  *  Copyright 2015 Google Inc. All rights reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  Licensed under the Apache License, Version 2.0 (the 'License');
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
  *    https://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License
@@ -53,8 +53,8 @@
               case 'installed':
                 // At this point, the old content will have been purged and the
                 // fresh content will have been added to the cache.
-                // It's the perfect time to display a "New content is
-                // available; please refresh." message in the page's interface.
+                // It's the perfect time to display a 'New content is
+                // available; please refresh.' message in the page's interface.
                 break;
 
               case 'redundant':
@@ -73,4 +73,21 @@
   }
 
   // Your custom JavaScript goes here
+  document.addEventListener('DOMContentLoaded', function() {
+    console.log('Com Loaded');
+
+    var header = document.getElementsByClassName('er-header');
+
+    console.log(header);
+
+    header.addEventListener('click', function() {
+      console.log('click', this);
+    });
+
+    var closeBtn = document.getElementsByClassName('er-close');
+
+    closeBtn.addEventListener('click', function() {
+      console.log('click', this);
+    });
+  });
 })();
